d8e988c36faf684f384370ce4e9622a71bab1e4c
Michael Wales <michaelwales@gmail.com>
2020-06-05 23:10:38 +0100

    Adds Case child class

---
+++ src/main/java/accessories/Case.java
@@ -0,0 +1,9 @@
+package accessories;
+
+public class Case extends Accessory{
+
+    public Case(String description, double costPrice, double salePrice) {
+        super(description, costPrice, salePrice);
+    }
+
+}
--- src/main/java/accessories/Cases.java
+++
@@ -1,9 +0,0 @@
-package accessories;
-
-public class Cases extends Accessory{
-
-    public Cases(String description, double costPrice, double salePrice) {
-        super(description, costPrice, salePrice);
-    }
-
-}
